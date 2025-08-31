aa3c40b9f3d115f754dda5b7893b0753b05497b4
UmsrAkne <sibata486@gmail.com>
2025-08-31 23:32:51 +0900

    refactor(viewmodel): move image loading logic into ImageItemViewModel and remove PreviewImageConverter
    - Deleted PreviewImageConverter and related XAML resource
    - Added async LoadAsync with preview/full resolution loading in ImageItemViewModel
    - Implemented cancellation support with CancellationTokenSource
    - Updated TabViewModel to call LoadAsync on SelectedItem change
    - Bound MainWindow Image.Source directly to SelectedItem.Image

--- PxViewer/ViewModels/TabViewModel.cs
+++ PxViewer/ViewModels/TabViewModel.cs
@@ -37,7 +37,15 @@ namespace PxViewer.ViewModels
         public ImageItemViewModel SelectedItem
         {
             get => selectedItem;
-            set => SetProperty(ref selectedItem, value);
+            set
+            {
+                selectedItem?.CancelLoad();
+                if (SetProperty(ref selectedItem, value))
+                {
+                    _ = selectedItem?.LoadAsync(previewMax: 800);
+                    selectedItem = value;
+                }
+            }
         }
 
         public object SelectedItemMeta { get; set; }
